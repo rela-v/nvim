ffcf5ec74d550cc9557673d2315624156fbf98e6
rela <rela.mngrbot@gmail.com>
2025-09-05 10:07:15 -0400

    removed python pathing

--- lua/core/plugin_config/init.lua
+++ lua/core/plugin_config/init.lua
@@ -1,3 +1,4 @@
+vim.g.python3_host_prog = vim.fn.expand("~/.venvs/nvim/bin/python")
 require("core.plugin_config.lualine")
 require("core.plugin_config.catppuccin")
 require("core.plugin_config.nvim-tree")
@@ -6,3 +7,6 @@ require("core.plugin_config.telescope")
 require("core.plugin_config.ultisnips")
 require("core.plugin_config.harpoon")
 require("core.plugin_config.commentnvim")
+vim.env.PATH = vim.env.PATH .. ':/opt/homebrew/bin'
+
+
